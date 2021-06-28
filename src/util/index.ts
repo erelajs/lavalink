30ced1cbd82f01747cc063a81f333b6883fb6e16
Solaris9 <warhammer4141@gmail.com>
2021-06-28 01:50:21 -0700

    Move lavalink provider to Erela's org

---
+++ src/util/index.ts
@@ -0,0 +1,7 @@
+export * from './constants';
+export * from './deepClone';
+export * from './enumerable';
+export * from './isObject';
+export * from './isPrimitive';
+export * from './mergeDefault';
+export * from './UtilityTypes';
